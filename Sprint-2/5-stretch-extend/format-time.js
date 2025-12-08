9c0680ee709d974d0c20f5c13c2ee777ec960b35
Yan <nubelaprofesora2021@gmail.com>
2025-12-08 15:42:11 +0100

    sprint2 4 5

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -4,8 +4,15 @@
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
+  const minutes = Number(time.slice(-2));
+  const minutesStr = String(minutes).padStart(2,"0");
+
   if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+    return `${hours - 12}:${minutesStr} pm`;
+  } else if (hours === 12){
+    return `12:${minutesStr} pm`;
+  } else if (hours === 0) {
+    return `12:${minutesStr} am`;
   }
   return `${time} am`;
 }
@@ -22,4 +29,18 @@ const targetOutput2 = "11:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
+);
+
+const currentOutput3 = formatAs12HourClock("12:05");
+const targetOutput3 = "12:05 pm";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("00:05");
+const targetOutput4 = "12:05 am";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
 );
