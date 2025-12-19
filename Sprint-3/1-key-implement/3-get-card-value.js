17d46e24c802729e8de91d8a44eccf567aba32de
Yan <nubelaprofesora2021@gmail.com>
2025-12-19 19:13:25 +0100

    sprint3-1

--- Sprint-3/1-key-implement/3-get-card-value.js
+++ Sprint-3/1-key-implement/3-get-card-value.js
@@ -8,7 +8,12 @@
 // write one test at a time, and make it pass, build your solution up methodically
 // just make one change at a time -- don't rush -- programmers are deep and careful thinkers
 function getCardValue(card) {
+    let rank = card.charAt(0);
+    let twoNumber = card.slice(0,2);
     if (rank === "A") return 11;
+    else if (rank < 10 && rank > 1) return Number(rank);
+    else if (twoNumber === "10" ||  rank === "J" || rank === "Q" || rank === "K") return 10;
+    else throw new Error ("Invalid card rank.");
 }
 
 // You need to write assertions for your function to check it works in different cases
@@ -33,19 +38,34 @@ assertEquals(aceofSpades, 11);
 // When the function is called with such a card,
 // Then it should return the numeric value corresponding to the rank (e.g., "5" should return 5).
 const fiveofHearts = getCardValue("5♥");
+assertEquals(fiveofHearts, 5);
 // ====> write your test here, and then add a line to pass the test in the function above
 
 // Handle Face Cards (J, Q, K):
 // Given a card with a rank of "10," "J," "Q," or "K",
 // When the function is called with such a card,
 // Then it should return the value 10, as these cards are worth 10 points each in blackjack.
+const numberTen = getCardValue("J♥");
+assertEquals(numberTen, 10);
 
 // Handle Ace (A):
 // Given a card with a rank of "A",
 // When the function is called with an Ace,
 // Then it should, by default, assume the Ace is worth 11 points, which is a common rule in blackjack.
 
+const ace = getCardValue("A♥");
+assertEquals(ace, 11);
+
 // Handle Invalid Cards:
 // Given a card with an invalid rank (neither a number nor a recognized face card),
 // When the function is called with such a card,
 // Then it should throw an error indicating "Invalid card rank."
+
+try {
+  getCardValue("%%");
+  fail("Should have thrown an error"); //fail() is a testing helper that marks the test as failed when the expected error did not occur.
+} catch (error) {
+  assertEquals(error.message, "Invalid card rank.");//error.message is a property of an Error object.
+}
+
+
