7aeb14b8a9d9dc78a61984cbe25cd82a547e89b0
Yan <nubelaprofesora2021@gmail.com>
2025-12-20 18:16:17 +0100

    sprint3

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,6 +1,20 @@
-function passwordValidator(password) {
-    return password.length < 5 ? false : true
+/*function passwordValidator(password) {
+    return password.length >= 5;
 }
+*/ 
 
+ function passwordValidator(password) {
+    const length = password.length >= 5;
+    const upperCase = /[A-Z]/.test(password) ;
+    const lowerCase = /[a-z]/.test(password);
+    const number = /[0-9]/.test(password);
+    const symbol = /[!#$%.*&]/.test(password);
+    let previousArray = [];
+    function addPassword (password) {
+        previousArray.push(password);
+    }
+    const unique = !previousArray.includes(password);
+    return length && upperCase && lowerCase && number && symbol && unique ; 
+}
 
 module.exports = passwordValidator;
